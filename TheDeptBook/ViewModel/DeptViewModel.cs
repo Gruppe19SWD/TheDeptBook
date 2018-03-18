0481ee0c9db85345d4f81d52f1f5f0f9f76574e6
Morten <mo.rt.en@hotmail.com>
2018-03-18 20:12:49 +0100

    Lidt flere comments

--- TheDeptBook/ViewModel/DeptViewModel.cs
+++ TheDeptBook/ViewModel/DeptViewModel.cs
@@ -12,14 +12,20 @@ namespace TheDeptBook.ViewModel
 {
     public class DeptViewModel : BaseViewModel
     {
-        //at den arver fra BaseViewModel er grundet en ideer til videre implementation hvor de forskellige views kører på forskellige viewmodels
-        public BaseViewModel ViewModel { get; set; }
+        //at den arver fra BaseViewModel er grundet en ide til videre implementation hvor de forskellige views kører på forskellige viewmodels
+        //og hvilket view der vises ville være bestemt af den property set under her
+       // public BaseViewModel ViewModel { get; set; }
+
+
+
         public DeptModel DM;
 
         public DeptViewModel(DeptModel _DM)
         {
             DM = _DM;
         }
+
+        //henter listerne der skal vises fra deptmodellen
         public List<Person> PersonList => DM.Persons;
         public List<Loan> SelectedPersonLoan => DM.SelectedPerson.Loans;
 
@@ -49,6 +55,7 @@ namespace TheDeptBook.ViewModel
                     DM.SelectedPerson = value;
                     DM.NameOfSelectedPerson = value.Name;
                     OnPropertyChanged("NameOfSelectedPerson");
+                    //ny selected person starter også en ny onPropertyChanged på lånet der vises i view 2
                     OnPropertyChanged("SelectedPersonLoan");
                 }
             }
